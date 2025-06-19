3a7300c18e4b338108711860c055e1754ff484c2
Nicolas Nuñez <nuneznicolas582@gmail.com>
2025-06-19 14:19:17 -0300

    fix hover color botones pending y completed

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -11,8 +11,8 @@ const TaskList = () => {
 
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-8 gap-4">
                 <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-1 rounded-full font-semibold">All</button>
-                <button className="bg-gray-600 hover:bg-gray-600 text-white px-4 py-1 rounded-full font-semibold">Pending</button>
-                <button className="bg-gray-600 hover:bg-gray-600 text-white px-4 py-1 rounded-full font-semibold">Completed</button>
+                <button className="bg-gray-600 hover:bg-gray-700 text-white px-4 py-1 rounded-full font-semibold">Pending</button>
+                <button className="bg-gray-600 hover:bg-gray-700 text-white px-4 py-1 rounded-full font-semibold">Completed</button>
                 <div className="justify-end ml-auto">
                     <Link to="/tasks/new" className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-full font-semibold flex items-center gap-2"> 
                         <PlusCircleIcon className="w-5 h-5" /> New Task 

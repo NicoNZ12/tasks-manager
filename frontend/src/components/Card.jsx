d4995886dc24ca57810cccdda3a3a262d09d5563
Nicolas Nuñez <nuneznicolas582@gmail.com>
2025-06-20 17:00:58 -0300

    funcionalidad basica para crear y obtener las tasks. Fix en controlador createTask

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,12 @@
 
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
 
-const Card = () => {
+const Card = ({task}) => {
   return (
     <div className="bg-white rounded-xl shadow-md p-6 max-w-md w-full transition hover:shadow-lg">
-      <h2 className="text-2xl font-semibold text-purple-700 mb-2">Programar frontend</h2>
-      <p className="text-sm text-gray-500 mb-4">19/06/2025</p>
-      <p className="text-gray-700 mb-6">Programar la interfaz gráfica de la app</p>
+      <h2 className="text-2xl font-semibold text-purple-700 mb-2">{task.title}</h2>
+      <p className="text-sm text-gray-500 mb-4">{task.createdAt}</p>
+      <p className="text-gray-700 mb-6">{task.description}</p>
       <div className="flex justify-end space-x-4">
         <button className="flex items-center gap-1 px-4 py-2 bg-purple-500 text-white rounded-full hover:bg-purple-600 transition">
           <PencilIcon className="w-4 h-4" />
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import { createTask } from "../api/ApiTask";
 
 const Form = () => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [completed, setCompleted] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!title || !description){
+            console.error("Title and description are required.");
+            return;
+        };
+
+        const newTask = {
+            title,
+            description,
+            completed
+        };
+
+        try {
+            console.log("Tarea a enviar:", newTask);
+            const response = await createTask(newTask);
+            console.log("Task created:", response);
+
+            // Limpia el formulario
+            setTitle("");
+            setDescription("");
+            setCompleted(false);
+        } catch (error) {
+            console.error("Error creating task:", error);
+        }
+    };
+
     return (
         <div className="max-w-2xl mx-auto bg-white p-10 rounded-xl shadow-2xl">
-            <form className="flex flex-col space-y-6 ">
+            <form className="flex flex-col space-y-6" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="title" className="text-lg block font-medium mb-1 text-start">Title</label>
                     <input
@@ -11,6 +45,8 @@ const Form = () => {
                         name="title"
                         placeholder="Task title"
                         className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
                     />
                 </div>
 
@@ -21,6 +57,8 @@ const Form = () => {
                         name="description"
                         placeholder="Task description"
                         className="w-full px-4 py-2 border rounded-md resize-none h-24 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
                     >
                     </textarea>
                 </div>
@@ -31,6 +69,8 @@ const Form = () => {
                         id="completed"
                         name="completed"
                         className="h-4 w-4 text-purple-600 accent-purple-600 focus:ring-purple-500"
+                        checked={completed}
+                        onChange={(e) => setCompleted(e.target.checked)}
                     />
                     <label htmlFor="completed" className="text-m">Complete task</label>
                 </div>
